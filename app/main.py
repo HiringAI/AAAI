9a24713f8593b216571e681786b3732b312f5275
kimhyun5u <22kimhyun5u@gmail.com>
2025-02-07 10:10:17 +0900

    update: main.py

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ app = FastAPI()
 # Azure OpenAI 설정
 openai.api_key = settings.openai_api_key
 # Azure OpenAI를 사용할 경우, 아래와 같이 설정
-openai.api_type = "azure"
+openai.api_type = settings.openai_api_type
 openai.api_base = settings.openai_endpoint
 openai.api_version = settings.openai_api_version
+
+
 
 @app.get("/")
 def read_root():
