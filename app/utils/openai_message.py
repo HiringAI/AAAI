b4d91f949cb2a63775e68dea4e185a84f52a222a
kimhyun5u <22kimhyun5u@gmail.com>
2025-02-10 09:05:29 +0900

    update: openai_messages.py - add comments

--- app/utils/openai_message.py
+++ app/utils/openai_message.py
@@ -3,9 +3,11 @@ class OpenAIMessage():
         self._system_message = ""
         self._user_messages = []
 
+    # prompt messages 의 시스템 메시지를 설정한다.
     def set_system_message(self, content):
         self._system_message = content
 
+    # prompt messages 의 유저 메시지를 추가한다.
     def add_user_message(self, message_type, content):
         if message_type == "text":
             self._user_messages.append({
@@ -22,6 +24,7 @@ class OpenAIMessage():
         else:
             raise ValueError("Invalid message type")
 
+    # prompt messages 를 반환한다.
     def get_messages(self):
         return [
             {"role": "system", "content": self._system_message},
